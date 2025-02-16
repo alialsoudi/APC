391ee0e8acbf56f42ad1813cdb521067105f30a3
alialsoudi <alialsoudi900@gmail.com>
2025-02-17 02:15:52 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,37 +1,38 @@
 import streamlit as st
+import base64
 
 st.title("كمية البوتاس")
 
-# مربع إدخال "معدل التحميل"
-loading_rate_str = st.text_input("معدل التحميل (طن/ساعة):")  # استخدام st.text_input
+# تحويل الصورة إلى base64 (إذا كنت تستخدم بايثون)
+with open("APC_LOGO.jpg", "rb") as image_file:  # افتح الصورة في وضع البايت
+    encoded_string = base64.b64encode(image_file.read()).decode()  # قم بتحويلها إلى base64
 
-# مربع إدخال "الموقع"
-location_input_str = st.text_input("الموقع (متر):")  # استخدام st.text_input
+# عرض الصورة في Streamlit0
+st.markdown(
+    f"""
+    <img src="data:image/png;base64,{encoded_string}" width="200">  """,  # تضمين الصورة في HTML
+    unsafe_allow_html=True,
+)
+
+loading_rate = st.number_input("معدل التحميل (طن/ساعة):", min_value=0.0)
+location_input = st.number_input("الموقع (متر):", min_value=0.0)
 
 if st.button("احسب"):
     try:
-        # التحقق من أن القيم المدخلة ليست فارغة
-        if loading_rate_str and location_input_str:
-            loading_rate = float(loading_rate_str)  # تحويل القيمة إلى float
-            location_input = float(location_input_str)  # تحويل القيمة إلى float
+        if loading_rate > 0 and location_input > 0:
+            BELT_SPEED = 2.75
+            POTASH_DENSITY = 1500
 
-            if loading_rate > 0 and location_input > 0:
-                BELT_SPEED = 2.75
-                POTASH_DENSITY = 1500
+            location = location_input + 100
+            loading_rate_kg_per_sec = loading_rate * 1000 / 3600
+            cross_sectional_area = loading_rate_kg_per_sec / (POTASH_DENSITY * BELT_SPEED)
+            remaining_potash_volume = cross_sectional_area * location
+            remaining_potash_mass = remaining_potash_volume * POTASH_DENSITY
 
-                location = location_input + 100
-                loading_rate_kg_per_sec = loading_rate * 1000 / 3600
-                cross_sectional_area = loading_rate_kg_per_sec / (POTASH_DENSITY * BELT_SPEED)
-                remaining_potash_volume = cross_sectional_area * location
-                remaining_potash_mass = remaining_potash_volume * POTASH_DENSITY
-
-                remaining_potash_tons = remaining_potash_mass / 1000
-                st.write(f"كمية البوتاس المتبقية: {remaining_potash_tons:.2f} طن")
-            else:
-                st.error("خطأ: يجب أن تكون جميع القيم المدخلة موجبة.")
-
+            remaining_potash_tons = remaining_potash_mass / 1000
+            st.write(f"كمية البوتاس المتبقية: {remaining_potash_tons:.2f} طن")
         else:
-            st.error("خطأ: يرجى إدخال قيم لمعدل التحميل والموقع.")  # رسالة خطأ جديدة
+            st.error("خطأ: يجب أن تكون جميع القيم المدخلة موجبة.")
 
     except ValueError:
         st.error("خطأ: يرجى إدخال أرقام صحيحة.")
